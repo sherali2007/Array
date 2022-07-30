bd08beba4b22b0baae7008c65ecedf8c86161a00
Ali <110012339+sherali2007@users.noreply.github.com>
2022-07-30 01:48:25 -0400

    Add files via upload

---
+++ Array/src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("number: ");
+        int number = sc.nextInt();
+        int[] arr = new int[number];
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max){
+                max = arr[i];
+            }
+        }
+        System.out.println("Max "+max);
+    }
+    }
